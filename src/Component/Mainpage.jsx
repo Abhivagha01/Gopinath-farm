d9acd41c2c65fb3e21dd45c4e22f320dbee423ff
Abhivagha01 <abhishekvaghasiya00@gmail.com>
2024-01-18 22:46:36 +0530

    some changes

--- src/Component/Mainpage.jsx
+++ src/Component/Mainpage.jsx
@@ -17,7 +17,12 @@ import s12 from '../../src/assets/image/saputara/tent City saputara.jpg'
 import s13 from '../../src/assets/image/sayan-olpad/Ladali villa.jpg'
 import s14 from '../../src/assets/image/wada/kavya villa wada.jpg'
 import { Link } from 'react-router-dom';
-
+import social1 from '../assets/image/social/facebook.png'
+import social2 from '../assets/image/social/instagram.png'
+import social3 from '../assets/image/social/s1.png'
+import social4 from '../assets/image/social/s2.png'
+import social5 from '../assets/image/social/s3.png'
+import social6 from '../assets/image/social/s4.png'
 
 
 function Mainpage() {
@@ -59,9 +64,7 @@ function Mainpage() {
                 </div>
               ))}
             </OwlCarousel>
-            <div className="logo">
-              <img src="" alt="" />
-            </div>
+
           </div>
 
         </div>
@@ -69,16 +72,50 @@ function Mainpage() {
         <div className="social-outer">
           <div className="container">
             <div className="social-inner">
+              <div className="social-icon m-2">
+                <ul className='d-flex  justify-content-center'>
+                  <li className='mx-2 my-2'>
+                    <Link to="https://www.facebook.com/people/Gopinathji-Farm-Surat/100091930071316/?mibextid=ZbWKwL">
+                      <img src={social1} alt="facebook" />
+                    </Link>
+                  </li>
+                  <li className='mx-2 my-2'>
+                    <Link to="https://www.instagram.com/gopinathji_farm_?igshid=NTc4MTIwNjQ2YQ%3D%3D">
+                      <img src={social2} alt="instagram" />
+                    </Link>
+                  </li>
+                  <li className='mx-2 my-2'>
+                    <Link to="mailto:farmsurat4@gmail.com">
+                      <img src={social3} alt="email" />
+                    </Link>
+                  </li>
+                  <li className='mx-2 my-2'>
+                    <Link to="tel:+91 90819 26237">
+                      <img src={social4} alt="call" />
+                    </Link>
+                  </li>
+                  <li className='mx-2 my-2'>
+                    <Link to="https://wa.me/9081926237">
+                      <img src={social5} alt="watshapp" />
+                    </Link>
+                  </li>
+                  <li className='mx-2 my-2'>
+                    <Link to="https://www.google.com/maps/place/Kanbhi,+Gujarat+394540/@21.3924,72.8342,17z/data=!4m6!3m5!1s0x3be036f162f6f0c3:0x7832a50bfcb78369!8m2!3d21.3923627!4d72.8341927!16s%2Fg%2F1hhw1wd9j?hl=en&entry=ttu">
+                      <img src={social6} alt="location" />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
               <div className="social-item">
-                <div className="item d-lg-flex justify-content-flex-start mb-2">
-                  <div className="icon">  <i class="fas fa-map-marker-alt"></i></div>
+                <div className="item d-flex align-items-center justify-content-flex-start mb-2">
+                  <div className="icon">  <i class="fas fa-map-marker-alt mb-2"></i></div>
                   <div className="text">Kanbhi, Gujarat 394540, India</div>
                 </div>
-                <div className="item d-lg-flex justify-content-flex-start mb-2">
+                <div className="item d-flex  align-items-center justify-content-flex-start mb-2">
                   <div className="icon">  <i class="fas fa-phone"></i></div>
                   <div className="text"><Link to="tel:+91 90819 26237">+91 90819 26237</Link></div>
                 </div>
-                <div className="item d-lg-flex justify-content-flex-start mb-2">
+                <div className="item d-flex  align-items-center justify-content-flex-start mb-2">
                   <div className="icon"> <i class="fas fa-envelope"></i></div>
                   <div className="text"><Link to="mailto:farmsurat4@gmail.com">farmsurat4@gmail.com</Link></div>
                 </div>
@@ -102,6 +139,52 @@ function Mainpage() {
                   <li>કોઈ પણ નુકસાન થશે તો અલગ ચાર્જ લેવામાં આવશે.</li>
                   <li>ગાર્ડનમાં  ફુલ કે પાન</li>
                 </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="farm-outer">
+          <div className="container">
+            <div className="section-title">
+              <div class="flag-discount">Our Farms</div>
+            </div>
+            <div className="farm-item d-flex flex-wrap my-2">
+              <div className="item">
+                <div className="item-inner">
+                  <div className="image-block">
+                    <img src={s1} alt="farm" />
+                  </div>
+                  <div className="farm-name">Malbar Hill Apartment</div>
+                  <div className="farm-button d-flex justify-content-between align-items-center">
+                    <div className="location"><i class="fas fa-globe"></i></div>
+                    <div className="booknow"><i class="fas fa-bookmark"></i>Book Now</div>
+                  </div>
+                </div>
+              </div>
+              <div className="item">
+                <div className="item-inner">
+                  <div className="image-block">
+                    <img src={s1} alt="farm" />
+                  </div>
+                  <div className="farm-name">Malbar Hill Apartment</div>
+                  <div className="farm-button d-flex justify-content-between align-items-center">
+                    <div className="location"><i class="fas fa-globe"></i></div>
+                    <div className="booknow"><i class="fas fa-bookmark"></i>Book Now</div>
+                  </div>
+                </div>
+              </div>
+              <div className="item">
+                <div className="item-inner">
+                  <div className="image-block">
+                    <img src={s1} alt="farm" />
+                  </div>
+                  <div className="farm-name">Malbar Hill Apartment</div>
+                  <div className="farm-button d-flex justify-content-between align-items-center">
+                    <div className="location"><i class="fas fa-globe"></i></div>
+                    <div className="booknow"><i class="fas fa-bookmark"></i>Book Now</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/Mainrutes.js
+++ src/Mainrutes.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { CiLocationOn } from "react-icons/ci";
+import Ahmedabad1 from '../src/assets/image/ahemedabad/ROYAL VILLA AD...jpg'
+import lichivilla1 from '../src/assets/image/lonavala/Lichi Villa.jpg'
+
 function Mainrutes() {
     const Farm = [
         {
             id: 1,
-            type: "surat",
+            type: "Ahmedabad",
             items: [
                 {
                     id: 1,
-                    Farm_name: "Malbar Hill Apartment",
-                    Farm_image: "https://lh3.googleusercontent.com/p/AF1QipPAMXTTQFnQL1mWXsr05OFqnbw794mWadiPpJan=w960-h960-n-o-k-v1",
-                    Address: "44, Causeway Rd, Merulaxmi Society, Rander, Surat, Gujarat 395005",
-                    Mobile_no: "90999 44478",
-                    location: 'https://www.google.com/maps/place/Malbar+Hill+Apartment/@21.2137676,72.7957691,17z/data=!3m1!4b1!4m20!1m13!4m12!1m4!2m2!1d72.910508!2d21.2197164!4e1!1m6!1m2!1s0x3be04c241b2bffff:0x6a68be9fc0a728d9!2smalabar+hill+surat!2m2!1d72.7983584!2d21.2137469!3m5!1s0x3be04c241b2bffff:0x6a68be9fc0a728d9!8m2!3d21.2137676!4d72.798344!16s%2Fg%2F11bzzyksvd?hl=en&entry=ttu',
+                    Farm_name: "Royal Villa",
+                    Farm_image: Ahmedabad1,
+                    Address: "Suyog Apt, Besides, nr. Zebar school, Thaltej, Ahmedabad, Gujarat 380058",
+                    Mobile_no: "90819 26237",
+                    location: 'https://www.google.com/maps/dir//Royal+Villa,+Suyog+Apt,+Besides,+nr.+Zebar+school,+Thaltej,+Ahmedabad,+Gujarat+380058/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x395e9b4bc72e2ee9:0x4b1c673125815726?sa=X&ved=2ahUKEwiqj9rHsOeDAxX_s1YBHYSAATkQ48ADegQIBBAA',
+                },
+            ],
+            id: 2,
+            type: "Lonavala",
+            items: [
+                {
+                    id: 1,
+                    Farm_name: "Lichi Villa",
+                    Farm_image: lichivilla1,
+                    Mobile_no: "90819 26237",
+                },
+                {
+                    id: 1,
+                    Farm_name: "Royal Villa",
+                    Farm_image: Ahmedabad1,
+                    Address: "Narayani Dham Road Lonavla, Maharashtra 410401 India",
+                    Mobile_no: "83800 00958",
+                    location: '',
                 },
             ]
+        
         }
 
     ];
@@ -48,8 +70,8 @@ function Mainrutes() {
                                     <div className="card-body">
                                         <h4 className="card-title mb-4">{item.Farm_name}</h4>
                                         <div className="d-lg-flex justify-content-between">
-                                            <button  onClick={() => openLocationInMaps(item.location)} style={{background:"red",border:"none"}}className="btn btn-primary bg-none"><CiLocationOn /></button>
-                                            <button  onClick={() => sendWhatsAppMessage(item.Farm_name, item.Address, item.Mobile_no, item.Farm_image)} className="btn btn-success">Book Now</button>
+                                            <button onClick={() => openLocationInMaps(item.location)} style={{ background: "red", border: "none" }} className="btn btn-primary bg-none"><CiLocationOn /></button>
+                                            <button onClick={() => sendWhatsAppMessage(item.Farm_name, item.Address, item.Mobile_no, item.Farm_image)} className="btn btn-success">Book Now</button>
                                         </div>
                                     </div>
                                 </div>
